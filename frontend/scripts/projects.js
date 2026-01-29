fb6f319702d11040034b3a9fb439e056f7c172f1
Gonçalo Pinto <goncalo.luis.pinto@gmail.com>
2026-01-29 00:38:19 +0000

    added filter by technologies tag on projects panel

--- frontend/scripts/projects.js
+++ frontend/scripts/projects.js
@@ -1,28 +1,74 @@
+let languages = [];
+let cachedProjects = [];
+
+document.addEventListener("DOMContentLoaded", async () => {
+    await loadProject();
+
+    render_projects(cachedProjects);
+});
+
 export async function loadProject() {
-    console.log("yeyyy");
     try {
-        const container = document.getElementById("my-projects"); 
-        if (!container) return;
+        const res = await fetch("../data/projects.json");
+        cachedProjects = await res.json();
 
-        const res = await fetch("http://localhost:8000/projects");
-        const projects = await res.json();
+        languages = [];
+        cachedProjects.forEach(p => {
+            if (p.languages) {
+                p.languages.forEach(lang => {
+                    if (!languages.includes(lang)) {
+                        languages.push(lang);
+                    }
+                });
+            }
+        });
 
-        container.innerHTML = ""; // limpa antes
+    } catch (err) {
+        console.error("Erro ao carregar projetos:", err);
+    };
+}
+
+function render_projects(projects) {
+    const container = document.getElementById("my-projects"); 
+    if (!container) return;
+
+    container.innerHTML = ""; // limpa antes
 
         projects.forEach(p => {
             const card = document.createElement("div");
+            const languagesHTML = p.languages 
+            ? p.languages.map(lang => `<span class="tag">${lang}</span>`).join(", ") 
+            : `<span class="tag">N/A</span>`;
 
             card.innerHTML = `
                 <h3>${p.name}</h3>
                 <p>${p.description}</p>
-                <p><strong>Linguagem:</strong> ${p.language ?? "N/A"}</p>
+                <p><strong>Linguagem:</strong></p>
+                <div class="tags">
+                    ${languagesHTML}
+                </div>
                 <a href="${p.url}" target="_blank">See on GitHub</a>
                 <hr>
             `;
 
             container.appendChild(card);
         });
-    } catch (err) {
-        console.error("Erro ao carregar projetos:", err);
-    };
 }
+
+document.getElementById("search").addEventListener("input", (e) => {
+    let projects = [];
+    const value = e.target.value.toLowerCase();
+
+    cachedProjects.forEach(project => {
+        if(!project.languages) return;
+
+        project.languages.forEach(lang => {
+            const isVisible = lang.toLowerCase().includes(value);
+            if(isVisible && !projects.includes(project)) {
+                projects.push(project);
+            }
+        })
+    });
+    
+    render_projects(projects);
+})
