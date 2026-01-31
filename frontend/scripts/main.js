75ca3e527f574d657acd8032118b29f3ac338d8d
Gonçalo Pinto <goncalo.luis.pinto@gmail.com>
2026-01-31 23:50:38 +0000

    added posts visualization on home-page

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,6 +1,9 @@
 import { loadProject } from "./projects.js";
 import { sendContacts } from "./contacts.js";
+import { initPosts } from "./posts.js";
+
 document.addEventListener("DOMContentLoaded", () => {
     loadProject();
     sendContacts();
+    initPosts();
 });
---
+++ frontend/scripts/posts.js
@@ -0,0 +1,64 @@
+export async function initPosts(){
+    const posts = await fetchPosts();
+
+    const container = document.getElementById("my-posts");
+    if (!container) return;
+
+    if (!posts || posts.length === 0) {
+        container.innerHTML = `<p>No posts yet</p>`;
+        return;
+    }
+
+    posts.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+    const recentPosts = posts.slice(0,3);
+
+    renderPosts(recentPosts);
+}
+
+async function fetchPosts(){
+    const res = await fetch("http://localhost:8000/post");
+
+    if(!res.ok){
+        console.log("Could not fetch the posts");
+        return;
+    }
+
+    const posts = await res.json();
+
+    return posts;
+}
+
+function renderPosts(posts){
+    if(!posts || posts.length === 0){
+        const container = document.getElementById("my-posts");
+
+        const card = document.createElement("div");
+        
+        card.innerHTML = `
+            <p>No posts for now</p>
+        `
+
+        container.appendChild(card);
+        return;
+    }
+
+    const container = document.getElementById("my-posts");
+
+    if(!container) return;
+
+    container.innerHTML = "";
+
+    recentPosts.forEach(p => {
+        const card = document.createElement("div");
+        
+        const date = new Date(p.created_at);
+
+        card.innerHTML = `
+            <h3>${p.title}</h3>
+            <p>${p.content}</p>
+            <p>${date}</p>
+        `
+        
+        container.appendChild(card);
+    });
+}
